a1066b2f8ec66f72ce3ad13a12bc3bacbe21d2ac
Abarham97 <abodzbarham@gmailcom>
2023-09-18 12:44:52 +0300

    lab14

---
+++ src/main/java/com/abarham97/codefellowship/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.abarham97.codefellowship;
+
+import com.abarham97.codefellowship.Repositry.UserSiteRepositry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserSiteRepositry UserSiteRepositry;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserSite usersite= UserSiteRepositry.findByUserName(username);
+
+        if (usersite == null) {
+            System.out.println("User not found " + username);
+            throw new UsernameNotFoundException("user" + username + " was not found in the db");
+        }
+        System.out.println("Found User: " + usersite.getUserName());
+
+     return (UserDetails) usersite;
+
+
+
+    }
+}
